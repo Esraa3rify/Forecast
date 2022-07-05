e9ff281247f4a20d5424027f25644f3a323159ad
Esraa <esraa3rify47@gmail.com>
2022-07-05 14:51:02 +0200

    NavagationError

--- app/src/main/java/com/example/ui/MainActivity.kt
+++ app/src/main/java/com/example/ui/MainActivity.kt
@@ -1,12 +1,12 @@
 package com.example.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import androidx.navigation.ui.setupWithNavController
-import com.example.ui.R
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -19,16 +19,21 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
 
-        navController=Navigation.findNavController(this, R.id.nav_host_fragment)
 
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
 
         bottom_nav.setupWithNavController(navController)
 
+        Log.d("Error","Error")
+        
         NavigationUI.setupActionBarWithNavController(this, navController)
+
+
+
     }
-
-
     override fun onSupportNavigateUp(): Boolean {
         return NavigationUI.navigateUp(navController,null)
     }
+
+
 }
